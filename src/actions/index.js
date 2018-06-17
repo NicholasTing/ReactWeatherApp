90b4758e77bcfe55d194865f89c066a24330e9fb
nicholasting92 <nicholasting92@gmail.com>
2018-06-17 23:06:15 +1000

    WeatherApp

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = 'e8ac6293dc6ea769ab2b8abb7223e236';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+    
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
